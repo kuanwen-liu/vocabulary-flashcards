3789a4a00abe33f0c2df9be3c8cf6741d648b54d
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2025-12-29 00:39:36 -0500

    Initial commit from Specify template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Flashcards</h1>
+      <p>Flashcards are a great way to learn new things.</p>
+    </div>
+  )
+}
+
+export default App
